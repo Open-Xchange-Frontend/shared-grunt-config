5140bea85655af2f42442490291d3a44746f1e74
Julian Bäume <julian@svg4all.de>
2014-04-25 13:27:18 +0200

    don't push during grunt-bump
    will do that manually

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,8 @@ module.exports = function (grunt) {
         bump: {
             options: {
                 files: ['package.json'],
-                createTag: false
+                createTag: false,
+                push: false
             }
         }
     });
