0d30330bd0b98265e5aa8bb5989a96c7b4a4c781
Julian Bäume <julian.baeume@open-xchange.com>
2014-06-04 14:37:07 +0200

    [dist:build] swap copy_dist and uglify tasks
    it's more safe to first copy files over and then run the uglify task.
    This prevents problems with JavaScript files copied in a copy:dist_* task,
    although they should have been copied during copy:build_*. Since in such
    a case (destinations are not unique) it's most likely, the result of the
    uglify task should "win the race", uglify task will be run last.

--- tasks/shared/dist.js
+++ tasks/shared/dist.js
@@ -83,7 +83,7 @@ module.exports = function (grunt) {
     });
 
     // build a distribution ready version of the ui
-    grunt.registerTask('dist:build', ['clean', 'checkDependencies:build', 'bower', 'build', 'uglify', 'copy_dist']);
+    grunt.registerTask('dist:build', ['clean', 'checkDependencies:build', 'bower', 'build', 'copy_dist', 'uglify']);
     //for backwards compatibility:
     grunt.registerTask('dist', 'build a distribution ready version (DEPRECATED, use dist:build)', function () {
         grunt.verbose.warn('Using "dist" task directly has been deprecated, use "dist:build" instead');
