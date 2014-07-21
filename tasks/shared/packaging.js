8baaeb8d7d5c134498473cf17773b82ff5a63cf7
Julian Bäume <julian@svg4all.de>
2014-07-21 13:43:23 +0200

    add grunt tasks to execute build helper scripts
    This will add a unified interface for packaging helper scripts. Allows more easy
    local packaging, if everything needed is installed.

---
+++ tasks/shared/packaging.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = function (grunt) {
+    if (!grunt.isPeerDependencyInstalled('grunt-exec')) {
+        return;
+    }
+
+    var sign_options = grunt.option('no-sign') === true ? ' -us -uc': '';
+
+    grunt.config.extend('exec', {
+        dpkg_build: {
+            cmd: 'dpkg-buildpackage -b' + sign_options,
+            cwd: 'dist/<%= pkg.name %>-<%= pkg.version %>/'
+        },
+        rpmbuild: {
+            cmd: 'cp dist/*.orig.tar.* ~/rpmbuild/SOURCES/; cp dist/*.spec ~/rpmbuild/SOURCES; rpmbuild -bb dist/<%= pkg.name %>.spec'
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('dpkg-buildpackage', 'create installable deb packages based on all packaging information', function () {
+        grunt.task.run(['dist:source', 'dist:dpkg-source', 'exec:dpkg_build']);
+    });
+
+   grunt.registerTask('rpm-build', 'create installable rpm packages based on all packaging information', function () {
+        grunt.task.run(['dist:source', 'dist:rpm', 'exec:rpmbuild']);
+    });
+};
