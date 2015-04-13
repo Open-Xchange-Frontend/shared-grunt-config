2f99d263e4a30147165d37434fa005d144b2acfb
Julian Bäume <julian.baeume@open-xchange.com>
2015-04-13 17:24:28 +0200

    fix build
    always run grunt before commit

--- tasks/shared/bower.js
+++ tasks/shared/bower.js
@@ -23,5 +23,5 @@ module.exports = function (grunt) {
         }}, grunt.config('checkDependencies'))
     );
 
-    grunt.registerTask('bower', ['checkDependencies:bower'])
+    grunt.registerTask('bower', ['checkDependencies:bower']);
 };
