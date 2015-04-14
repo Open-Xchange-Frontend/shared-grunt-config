963b4e508eec4dda1d2984abb9ebb3621b34ce64
Julian Bäume <julian@svg4all.de>
2015-04-14 19:39:26 +0200

    handle MODULE_NOT_FOUND exception for npm and bower install
    this is an issue especially starting with node 0.12, where the default
    behaviour concerning global modules has changed. This might also be an
    issue with rare node 0.10 setups, though I wasn't able to provide a
    test-case for this.

--- tasks/shared/repair.js
+++ tasks/shared/repair.js
@@ -2,6 +2,17 @@
 
 module.exports = function (grunt) {
 
+    function handleModuleException (e) {
+        if (e.code === 'MODULE_NOT_FOUND') {
+            grunt.log.error(e.toString());
+            grunt.log.writeln('If installed globally, try to set environment variable NODE_PATH to your global node_modules directory');
+            //TODO: windows?
+            grunt.log.writeln('To find out the correct path, you can run `npm prefix -g` and append `lib/node_modules`');
+            grunt.log.writeln('To combine the last hints on Linux/Mac, use: `export NODE_PATH="$(npm prefix -g)/lib/node_modules:$NODE_PATH"`');
+        }
+        grunt.fail.fatal('Module needed in order to continue, make sure it is installed globally and NODE_PATH is set.');
+    }
+
     grunt.registerTask('repair:check', 'Check for several known problems', function () {
         grunt.task.run(['checkDependencies']);
     });
@@ -12,10 +23,14 @@ module.exports = function (grunt) {
     });
     grunt.registerTask('repair:bower_install', 'Run bower install', function () {
         var done = this.async();
-        var bower = require('bower');
-        bower.commands
-            .install([], { silent: true })
-            .on('end', done);
+        try {
+            var bower = require('bower');
+            bower.commands
+                .install([], { silent: true })
+                .on('end', done);
+        } catch (e) {
+            handleModuleException(e);
+        }
     });
 
     grunt.registerTask('repair:npm', 'Remove node_modules directory and run `npm install`', function () {
@@ -24,10 +39,14 @@ module.exports = function (grunt) {
     });
     grunt.registerTask('repair:npm_install', 'Run npm install', function () {
         var done = this.async();
-        var npm = require('npm');
-        npm.load({ silent: true }, function () {
-            npm.commands.install([], done);
-        });
+        try {
+            var npm = require('npm');
+            npm.load({ silent: true }, function () {
+                npm.commands.install([], done);
+            });
+        } catch (e) {
+            handleModuleException(e);
+        }
     });
 
     grunt.registerTask('repair', 'Try hard to get the setup into a usable state.', function () {
