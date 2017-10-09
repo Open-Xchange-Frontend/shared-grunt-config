6c8f018374b3bf84ffd75e0935764d2c5e60acf0
Julian Bäume <julian@svg4all.de>
2017-10-09 16:58:13 +0200

    Fix problem with source packages built from latest generator template
    the lint task can never run, because the linter configuration was not a
    part of the source package.
    
    Fixes Open-Xchange-Frontend/generator-ox-ui-module#4

--- tasks/shared/source_pkg.js
+++ tasks/shared/source_pkg.js
@@ -15,6 +15,7 @@ module.exports = function (grunt) {
                         'Gruntfile.js',
                         '.jshintrc',
                         'bower.json',
+                        '.eslintrc',
                         'package.json'
                     ],
                     dot: true,
